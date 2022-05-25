b91561f083bf4dfa3afae8422492b943831e4225
Felipe Albuquerque <felipeqwe65@gmail.com>
2022-05-25 13:26:06 -0300

    Initial Commit

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('./admin')
+
 module.exports = app => {
 
     // Com o uso do consign, no momento que for carregado os arquivos no index.js será obtido de forma 
@@ -8,39 +10,51 @@ module.exports = app => {
     
     
     app.route('/users')
-        .post(app.api.user.save)
-        .get(app.api.user.get)
+        .all(app.config.passport.authenticate())
+        .post(admin(app.api.user.save))
+        .get(admin(app.api.user.get))
 
 
     app.route('/users/:id')
-        .put(app.api.user.save)
-        .get(app.api.user.getById)
+        .all(app.config.passport.authenticate())
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById))
 
     app.route('/categories')
-        .get(app.api.category.get)
-        .post(app.api.category.save)
+        .all(app.config.passport.authenticate())
+        .get(admin(app.api.category.get))
+        .post(admin(app.api.category.save))
 
     /* 
     A ordem das urls é importante para o express, então, getTree tem que vir antes de
     categories/:id
     */
     app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
     
     app.route('/categories/:id')
+        .all(app.config.passport.authenticate())    
         .get(app.api.category.getById) 
-        .put(app.api.category.save)
-        .delete(app.api.category.remove) 
+        .put(admin(app.api.category.save))
+        .delete(admin(app.api.category.remove)) 
 
     app.route('/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.get)
-        .post(app.api.article.save)
+        .post(admin(app.api.article.save))
     
     app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getById)
-        .put(app.api.article.save)
-        .delete(app.api.article.remove)
+        .put(admin(app.api.article.save))
+        .delete(admin(app.api.article.remove))
     
     app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory) 
+
+    app.route('/stats')
+        .all(app.config.passport.authenticate())
+        .get(app.api.stat.get)
 } 
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,22 @@
 const app = require('express')()
 const db = require('./config/db')
+const mongoose = require('mongoose')
+
+require('./config/mongodb')
 
 // Tendo em vista o knex já configurado pro db correto e importado no index, é possível
 // usar o app.db para fazer as querys no db
 app.db = db
-
+app.mongoose = mongoose
 // Utilizei o consign para ajudar a fazer as dependências dentro da aplicação e não precisar 
 // utilizar todos os require de todos os arquivos da aplicação
 const consign = require('consign')
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
-    .then('./api/validation.js')
+    .then('./api/validation.js') 
     .then('./api') 
+    .then('./schedule')
     .then('./config/routes.js')
     
     // injeta em cada uma das dependências que vão ser carregadas, como parâmetro, no app
